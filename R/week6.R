bf7c90250c147913f99a5635860dad6915bec02c
Vivien Lee <vivienlvv@gmail.com>
2023-02-22 22:15:57 -0600

    Part 2 - Data Import and Cleaning

--- R/week6.R
+++ R/week6.R
@@ -1,2 +1,12 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(stringi)
+library(stringr)
+
+
+
+# Data Import 
+citations = stri_read_lines("../data/citations.txt", encoding = "WINDOWS-1252") # Need to double check
+citations_txt = citations[!stri_isempty(citations)]
+length(citations) - length(citations_txt)
+mean(str_length(citations_txt))
