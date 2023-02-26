a93428e5df1c198119426c2f4e5ca533785ec270
Vivien Lee <vivienlvv@gmail.com>
2023-02-26 14:01:03 -0600

    Modified line 22 to capture editions in chapters

--- R/week6.R
+++ R/week6.R
@@ -19,6 +19,6 @@ citations_tbl = tibble(line = 1:length(citations_txt), cite = citations_txt) %>%
   mutate(year = str_match(cite, pattern = "\\((\\d{4})\\)\\.")[,2]) %>% # Question: Return NA if doesn't follow APA format?
   mutate(page_start = str_match(cite, pattern = "(\\d+)\\-\\d+")[,2]) %>% 
   mutate(perf_ref = str_detect(cite, fixed("performance", ignore = TRUE))) %>% 
-  mutate(title = str_match(cite, pattern = "\\(\\d{4}\\)\\.\\s*([A-Z][^.]*)")[,2]) %>% # Revisit
+  mutate(title = str_match(cite, pattern = "\\(\\d{4}\\)\\.\\s*([A-Z](?:\\(.*\\)|[^.])*)")[,2]) %>% # Old pattern: "\\(\\d{4}\\)\\.\\s*([A-Z][^.]*)"; still need to deal with non-period endings
   mutate(first_author = str_match(cite, pattern = "^([\\w|\\-]+,\\s(?:[A-Z]\\.\\s*)+)[,|\\s\\(\\d{4}\\)]+")[,2]) # explore non-capture and char class next time; first author old pattern = "^(\\w+,\\s(?:[A-Z]\\.\\s*)+)[,|\\s\\(\\d{4}\\)]+", new pattern matched additional 452 people
 sum(!is.na(citations_tbl$first_author))
