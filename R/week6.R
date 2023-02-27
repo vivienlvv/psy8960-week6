a6628f74f985fcb81949d780a360ef81e2ee6281
Vivien Lee <vivienlvv@gmail.com>
2023-02-27 10:54:41 -0600

    Modified Line 22 to match titles ending with non-periods.

--- R/week6.R
+++ R/week6.R
@@ -19,7 +19,7 @@ citations_tbl = tibble(line = 1:length(citations_txt), cite = citations_txt) %>%
   mutate(year = str_match(cite, pattern = "\\((\\d{4})\\)\\.")[,2]) %>% 
   mutate(page_start = str_match(cite, pattern = "(\\d+)\\-\\d+")[,2]) %>% 
   mutate(perf_ref = str_detect(cite, fixed("performance", ignore = TRUE))) %>% 
-  mutate(title = str_match(cite, pattern = "\\(\\d{4}\\)\\.\\s*([A-Z](?:\\(.*\\)|[^.])*)")[,2]) %>% 
+  mutate(title = str_match(cite, pattern = "\\(\\d{4}\\)\\.\\s([A-Z][^.]+[?!.])\\s[A-Z]")[,2]) %>% 
   mutate(first_author = str_extract(cite, pattern = "^(?:([A-Z][\\w\\-]+,\\s[A-Z\\.\\s*]+)|([A-Z][\\w\\s]+)\\.)(?=,|\\s\\(\\d{4}\\))")) 
 
 sum(!is.na(citations_tbl$first_author))
