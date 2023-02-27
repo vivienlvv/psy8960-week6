819768bd87172354e26641233cae6022da9bbf8f
Vivien Lee <vivienlvv@gmail.com>
2023-02-26 23:13:45 -0600

    Removed comments and unnecessary lines

--- R/week6.R
+++ R/week6.R
@@ -6,7 +6,7 @@ library(tidyverse)
 
 
 # Data Import 
-citations = stri_read_lines("../data/citations.txt", encoding = "WINDOWS-1252") # Need to double check
+citations = stri_read_lines("../data/citations.txt", encoding = "WINDOWS-1252") 
 citations_txt = citations[!stri_isempty(citations)]
 length(citations) - length(citations_txt)
 mean(str_length(citations_txt))
@@ -19,7 +19,7 @@ citations_tbl = tibble(line = 1:length(citations_txt), cite = citations_txt) %>%
   mutate(year = str_match(cite, pattern = "\\((\\d{4})\\)\\.")[,2]) %>% 
   mutate(page_start = str_match(cite, pattern = "(\\d+)\\-\\d+")[,2]) %>% 
   mutate(perf_ref = str_detect(cite, fixed("performance", ignore = TRUE))) %>% 
-  mutate(title = str_match(cite, pattern = "\\(\\d{4}\\)\\.\\s*([A-Z](?:\\(.*\\)|[^.])*)")[,2]) %>% # Old pattern: "\\(\\d{4}\\)\\.\\s*([A-Z][^.]*)"; still need to deal with non-period endings
-  mutate(first_author_new = str_extract(cite, pattern = "^(?:([A-Z][\\w\\-]+,\\s[A-Z\\.\\s*]+)|([A-Z][\\w\\s]+)\\.)(?=,|\\s\\(\\d{4}\\))")) %>%  # explore non-capture and char class next time; first author old pattern = "^(\\w+,\\s(?:[A-Z]\\.\\s*)+)[,|\\s\\(\\d{4}\\)]+", new pattern matched additional 452 people; old2 = "^([\\w|\\-]+,\\s(?:[A-Z]\\.\\s*)+)[,|\\s\\(\\d{4}\\)]+"
-  mutate(first_author_old = str_match(cite, "^([\\w|\\-]+,\\s(?:[A-Z]\\.\\s*)+)[,|\\s\\(\\d{4}\\)]+")[,2])
+  mutate(title = str_match(cite, pattern = "\\(\\d{4}\\)\\.\\s*([A-Z](?:\\(.*\\)|[^.])*)")[,2]) %>% 
+  mutate(first_author = str_extract(cite, pattern = "^(?:([A-Z][\\w\\-]+,\\s[A-Z\\.\\s*]+)|([A-Z][\\w\\s]+)\\.)(?=,|\\s\\(\\d{4}\\))")) 
+
 sum(!is.na(citations_tbl$first_author))
