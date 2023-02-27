4c56490df987d8203c2c0a0e2200425e93e790cc
Vivien Lee <vivienlvv@gmail.com>
2023-02-26 22:54:41 -0600

    Edited Line 23 to capture first author before "and"; Deleted some comments

--- R/week6.R
+++ R/week6.R
@@ -12,14 +12,14 @@ length(citations) - length(citations_txt)
 mean(str_length(citations_txt))
 
 # Data Cleaning 
-sample(citations_txt, 10) # citations or citations_txt?
+sample(citations_txt, 10) 
 
 citations_tbl = tibble(line = 1:length(citations_txt), cite = citations_txt) %>%
-  mutate(cite = str_replace_all(cite, pattern = "\"|\'", replacement = "")) %>% # Question: why don't we need double escape
-  mutate(year = str_match(cite, pattern = "\\((\\d{4})\\)\\.")[,2]) %>% # Question: Return NA if doesn't follow APA format?
+  mutate(cite = str_replace_all(cite, pattern = "\"|\'", replacement = "")) %>% 
+  mutate(year = str_match(cite, pattern = "\\((\\d{4})\\)\\.")[,2]) %>% 
   mutate(page_start = str_match(cite, pattern = "(\\d+)\\-\\d+")[,2]) %>% 
   mutate(perf_ref = str_detect(cite, fixed("performance", ignore = TRUE))) %>% 
   mutate(title = str_match(cite, pattern = "\\(\\d{4}\\)\\.\\s*([A-Z](?:\\(.*\\)|[^.])*)")[,2]) %>% # Old pattern: "\\(\\d{4}\\)\\.\\s*([A-Z][^.]*)"; still need to deal with non-period endings
-  mutate(first_author = str_extract(cite, pattern = "^(?:([A-Z][\\w\\-]+,\\s[A-Z\\.\\s*]+)|([A-Z][\\w\\s]+)\\.)(?=,|\\s\\(\\d{4}\\))"))  # explore non-capture and char class next time; first author old pattern = "^(\\w+,\\s(?:[A-Z]\\.\\s*)+)[,|\\s\\(\\d{4}\\)]+", new pattern matched additional 452 people; old2 = "^([\\w|\\-]+,\\s(?:[A-Z]\\.\\s*)+)[,|\\s\\(\\d{4}\\)]+"
-
+  mutate(first_author_new = str_extract(cite, pattern = "^(?:([A-Z][\\w\\-]+,\\s[A-Z\\.\\s*]+)|([A-Z][\\w\\s]+)\\.)(?=,|\\s\\(\\d{4}\\))")) %>%  # explore non-capture and char class next time; first author old pattern = "^(\\w+,\\s(?:[A-Z]\\.\\s*)+)[,|\\s\\(\\d{4}\\)]+", new pattern matched additional 452 people; old2 = "^([\\w|\\-]+,\\s(?:[A-Z]\\.\\s*)+)[,|\\s\\(\\d{4}\\)]+"
+  mutate(first_author_old = str_match(cite, "^([\\w|\\-]+,\\s(?:[A-Z]\\.\\s*)+)[,|\\s\\(\\d{4}\\)]+")[,2])
 sum(!is.na(citations_tbl$first_author))
